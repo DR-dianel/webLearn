ceb8b140af8b5dee6d628e253bfd63d170f8feaa
Raymond <raymond_diao@163.com>
2021-01-12 16:27:49 +0800

    开始学习 TypeScript

---
+++ TYPESCRIPT-BASIC/basic-types.ts
@@ -0,0 +1,12 @@
+let isDone:boolean = false
+
+let age:nnumber = 10
+
+let firstName:string  = 'Raymond'
+
+let message:string = "Hello, ${firstName}"
+
+// 明确数据类型时 尽量不要使用
+let notSure:any = 4
+notSure = 'maybe a string'
+notSure = true
